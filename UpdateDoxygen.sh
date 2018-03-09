d22f5777327c54a41e5fb3f6ba6ec598205d1ec0
Jonathan Dickerson <jonathan.dickerson@siu.edu>
2018-03-09 11:35:47 -0600

    Added a script to automatically generate doxygen files and push them to github.

---
+++ UpdateDoxygen.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "This script runs doxygen and pushes the results to github. If no arguements are supplied to this script, the script does not print the results of doxygen to the terminal. However, if doxygen output is desired,
+run the script with any number as an argument, and doxygen output will be printed to the terminal."
+echo 
+echo
+echo Changing to docs folder.
+cd docs/
+
+echo Running Doxygen.
+
+if [ "$#" -ne 1 ]; then
+	doxygen Doxyfile &> /dev/null 
+else
+	doxygen Doxyfile
+fi
+
+echo Changing to html directory.
+cd ../html/
+
+echo Pushing to git.
+
+git push origin gh-pages
